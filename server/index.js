4c084657f0183beba540ae5fbd293537a3aa2435
AIwithRajesh <choudharyrajesh729@gmail.com>
2025-08-26 22:56:01 +0530

    update: created resolver to get data

--- server/index.js
+++ server/index.js
@@ -6,29 +6,41 @@ import cors from "cors";
 
 async function startServer() {
   const app = express();
-  const server = new ApolloServer({
-    typeDefs: `
-        type Todo {
-            id: ID!
-            title: String!
-            completed: Boolean
-        }
-        
-        type Query {
-            getTodos: [Todo]
-        }
-    `,
-    resolvers: {},
-  });
 
-  app.use(bodyParser.json);
-  app.use(cors);
+  const typeDefs = `
+    type Todo {
+      id: ID!
+      title: String!
+      completed: Boolean
+    }
+    
+    type Query {
+      getTodos: [Todo]
+    }
+  `;
+
+  const resolvers = {
+    Query: {
+      getTodos: async () => {
+        const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+        if (!res.ok) {
+          return "Error";
+        }
+        const data = res.json();
+        return data;
+      },
+    },
+  };
+
+  const server = new ApolloServer({ typeDefs, resolvers });
 
   await server.start();
 
-  app.use("/graphql", expressMiddleware(server));
+  app.use("/graphql", cors(), bodyParser.json(), expressMiddleware(server));
 
-  app.listen(8000, () => console.log("Server started server"));
+  app.listen(8000, () =>
+    console.log("🚀 Server ready at http://localhost:8000/graphql")
+  );
 }
 
 startServer();
